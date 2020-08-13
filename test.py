7bf72294bb2cad9f2f2069c3b7683078d7e7964b
chris_linux <buswinka@g.harvard.edu>
2020-08-13 15:07:43 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import plotarchive as pa
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+import os
+
+print('getcwd:      ', os.getcwd())
+print('__file__:    ', __file__)
+
+# plt.plot([1,2,3], [1,2,3], 'ko')
+# files, ax, fig = pa.archive('yeet')
+# fig.show()
+# print(ax)
+
+
+@pa.archiver(filename='test.plotarchive')
+def plot(x,y):
+    plt.plot(x,y)
+    plt.show()
+    return None
+
+
+plot([1,2,3], np.array([1,2,3]), )
